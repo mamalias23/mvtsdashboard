2ec30428ac3d5a7dfaea5213931abbfd8f6a491f
Elias C Mamalias <emamalias@Eliass-iMac.local>
2015-05-26 22:32:06 +0800

    fix announcement

--- app/views/announcements/edit.blade.php
+++ app/views/announcements/edit.blade.php
@@ -81,10 +81,10 @@
                     <div id="tree">
                         <ul>
                             <li>
-                                <input type="checkbox" id="all" name="group[]" value="all" {{ array_key_exists('all', $groups) ? 'checked="true"':'' }} /><label for="all"><b>All</b></label>
+                                <input type="checkbox" id="all" name="group[]" value="all" {{ $announcement->receivers_group && array_key_exists('all', $groups) ? 'checked="true"':'' }} /><label for="all"><b>All</b></label>
                                 <ul>
                                     <li class="collapsed">
-                                        <input type="checkbox" id="school_records_personel" name="group[]" value="school_records_personel" {{ array_key_exists('school_records_personel', $groups) ? 'checked="true"':'' }} /><label for="school_records_personel"><b>School Records Personel</b></label>
+                                        <input type="checkbox" id="school_records_personel" name="group[]" value="school_records_personel" {{ $announcement->receivers_group && array_key_exists('school_records_personel', $groups) ? 'checked="true"':'' }} /><label for="school_records_personel"><b>School Records Personel</b></label>
                                         @if(SchoolYear::getActivated()->records_personel()->get()->count())
                                             <ul>
                                                 @foreach(SchoolYear::getActivated()->records_personel()->get() as $personel)
@@ -106,7 +106,7 @@
                                         @endif
                                     </li>
                                     <li class="collapsed">
-                                        <input type="checkbox" id="department_heads" name="group[]" value="department_heads" {{ array_key_exists('department_heads', $groups) ? 'checked="true"':'' }} /><label for="department_heads"><b>Department Heads</b></label>
+                                        <input type="checkbox" id="department_heads" name="group[]" value="department_heads" {{ $announcement->receivers_group && array_key_exists('department_heads', $groups) ? 'checked="true"':'' }} /><label for="department_heads"><b>Department Heads</b></label>
                                         @if(SchoolYear::getActivated()->departments()->get()->count())
                                             <ul>
                                                 @foreach(SchoolYear::getActivated()->departments()->get() as $department)
@@ -130,7 +130,7 @@
                                         @endif
                                     </li>
                                     <li class="collapsed">
-                                        <input type="checkbox" id="teachers" name="group[]" value="teachers" {{ array_key_exists('teachers', $groups) ? 'checked="true"':'' }} /><label for="teachers"><b>Teachers</b></label>
+                                        <input type="checkbox" id="teachers" name="group[]" value="teachers" {{ $announcement->receivers_group && array_key_exists('teachers', $groups) ? 'checked="true"':'' }} /><label for="teachers"><b>Teachers</b></label>
                                         @if(SchoolYear::getActivated()->teachers()->get()->count())
                                             <ul>
                                                 @foreach(SchoolYear::getActivated()->teachers()->get() as $teacher)
@@ -152,7 +152,7 @@
                                         @endif
                                     </li>
                                     <li class="collapsed">
-                                        <input type="checkbox" id="registrars" name="group[]" value="registrars" {{ array_key_exists('registrars', $groups) ? 'checked="true"':'' }} /><label for="registrars"><b>Registrars</b></label>
+                                        <input type="checkbox" id="registrars" name="group[]" value="registrars" {{ $announcement->receivers_group && array_key_exists('registrars', $groups) ? 'checked="true"':'' }} /><label for="registrars"><b>Registrars</b></label>
                                         @if(SchoolYear::getActivated()->registrars()->get()->count())
                                             <ul>
                                                 @foreach(SchoolYear::getActivated()->registrars()->get() as $registrar)
@@ -174,7 +174,7 @@
                                         @endif
                                     </li>
                                     <li class="collapsed">
-                                        <input type="checkbox" id="guards" name="group[]" value="guards" {{ array_key_exists('guards', $groups) ? 'checked="true"':'' }} /><label for="guards"><b>Guards</b></label>
+                                        <input type="checkbox" id="guards" name="group[]" value="guards" {{ $announcement->receivers_group && array_key_exists('guards', $groups) ? 'checked="true"':'' }} /><label for="guards"><b>Guards</b></label>
                                         @if(SchoolYear::getActivated()->guards()->get()->count())
                                             <ul>
                                                 @foreach(SchoolYear::getActivated()->guards()->get() as $guard)
@@ -196,7 +196,7 @@
                                         @endif
                                     </li>
                                     <li class="collapsed">
-                                        <input type="checkbox" id="other_staffs" name="group[]" value="other_staffs" {{ array_key_exists('other_staffs', $groups) ? 'checked="true"':'' }} /><label for="other_staffs"><b>Other Staffs</b></label>
+                                        <input type="checkbox" id="other_staffs" name="group[]" value="other_staffs" {{ $announcement->receivers_group && array_key_exists('other_staffs', $groups) ? 'checked="true"':'' }} /><label for="other_staffs"><b>Other Staffs</b></label>
                                         @if(SchoolYear::getActivated()->staffs()->get()->count())
                                             <ul>
                                                 @foreach(SchoolYear::getActivated()->staffs()->get() as $staff)
@@ -218,22 +218,22 @@
                                         @endif
                                     </li>
                                     <li class="collapsed">
-                                        <input type="checkbox" id="students" name="group[]" value="students" {{ array_key_exists('students', $groups) ? 'checked="true"':'' }} /><label for="students"><b>Students</b></label>
+                                        <input type="checkbox" id="students" name="group[]" value="students" {{ $announcement->receivers_group && array_key_exists('students', $groups) ? 'checked="true"':'' }} /><label for="students"><b>Students</b></label>
                                         @if(SchoolYear::getActivated()->curriculums()->get()->count())
                                             <ul>
                                                 @foreach(SchoolYear::getActivated()->curriculums()->get() as $curriculum)
                                                     <li class="collapsed">
-                                                        <input type="checkbox" id="personel-{{ $curriculum->id  }}" name="group[]" value="student_curriculum_{{ $curriculum->id  }}" {{ array_key_exists('student_curriculum_' . $curriculum->id, $groups) ? 'checked="true"':'' }} /><label for="personel-{{ $curriculum->id  }}"><b>{{ $curriculum->name }}</b></label>
+                                                        <input type="checkbox" id="personel-{{ $curriculum->id  }}" name="group[]" value="student_curriculum_{{ $curriculum->id  }}" {{ $announcement->receivers_group && array_key_exists('student_curriculum_' . $curriculum->id, $groups) ? 'checked="true"':'' }} /><label for="personel-{{ $curriculum->id  }}"><b>{{ $curriculum->name }}</b></label>
                                                         @if($curriculum->year_levels()->get()->count())
                                                             <ul>
                                                                 @foreach($curriculum->year_levels()->orderBy('level')->get() as $year)
                                                                     <li class="collapsed">
-                                                                        <input type="checkbox" id="personel-{{ $year->id  }}" name="group[]" value="student_year_{{ $year->id  }}" {{ array_key_exists('student_year_' . $year->id, $groups) ? 'checked="true"':'' }} /><label for="personel-{{ $year->id  }}"><b>{{ $year->description }}</b></label>
+                                                                        <input type="checkbox" id="personel-{{ $year->id  }}" name="group[]" value="student_year_{{ $year->id  }}" {{ $announcement->receivers_group && array_key_exists('student_year_' . $year->id, $groups) ? 'checked="true"':'' }} /><label for="personel-{{ $year->id  }}"><b>{{ $year->description }}</b></label>
                                                                         @if($year->sections()->get()->count())
                                                                             <ul>
                                                                                 @foreach($year->sections()->orderBy('name')->get() as $section)
                                                                                     <li class="collapsed">
-                                                                                        <input type="checkbox" id="personel-{{ $section->id  }}" name="group[]" value="student_section_{{ $section->id  }}" {{ array_key_exists('student_section_' . $section->id, $groups) ? 'checked="true"':'' }} /><label for="personel-{{ $section->id  }}"><b>{{ $section->name }}</b></label>
+                                                                                        <input type="checkbox" id="personel-{{ $section->id  }}" name="group[]" value="student_section_{{ $section->id  }}" {{ $announcement->receivers_group && array_key_exists('student_section_' . $section->id, $groups) ? 'checked="true"':'' }} /><label for="personel-{{ $section->id  }}"><b>{{ $section->name }}</b></label>
                                                                                         @if($section->students()->get()->count())
                                                                                             <ul>
                                                                                                  @foreach($section->students()->get() as $student)
