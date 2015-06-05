9ba81f76eedbb9c5c4986bd1ae05bbcfdbb02b7a
Elias C Mamalias <emamalias@Eliass-iMac.local>
2015-06-05 22:38:02 +0800

    dasdasd

--- app/views/dashboard.blade.php
+++ app/views/dashboard.blade.php
@@ -25,7 +25,47 @@
       </div>
     </div>
     <div class="box-body">
-      Under Construction
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="small-box bg-red">
+                    <div class="inner">
+                        <h3>{{ Student::where('school_year_id', SchoolYear::getActivated()->id)->get()->count() }}</h3>
+                        <p>Total Students</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-person-add"></i>
+                    </div>
+                    <a href="{{ route('backend.school-year.students.index', array(SchoolYear::getActivated()->id)) }}" class="small-box-footer">View all <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+
+            <div class="col-sm-4">
+                <div class="small-box bg-green">
+                    <div class="inner">
+                        <h3>{{ Teacher::where('school_year_id', SchoolYear::getActivated()->id)->get()->count() }}</h3>
+                        <p>Total Teachers</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-person-add"></i>
+                    </div>
+                    <a href="{{ route('backend.school-year.teachers.index', array(SchoolYear::getActivated()->id)) }}" class="small-box-footer">View all <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+
+            <div class="col-sm-4">
+                <div class="small-box bg-yellow">
+                    <div class="inner">
+                        <h3>{{ SchoolRecordPersonel::where('school_year_id', SchoolYear::getActivated()->id)->get()->count() }}</h3>
+                        <p>Total Personels</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-person-add"></i>
+                    </div>
+                    <a href="{{ route('backend.school-year.personels.index', array(SchoolYear::getActivated()->id)) }}" class="small-box-footer">View all <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+        </div>
+
     </div><!-- /.box-body -->
   </div>
   <!-- /.box -->
