c85a4b7e5034abbfca2044d4d47f74be1214afbc
Elias C Mamalias <emamalias@Eliass-iMac.local>
2015-06-06 08:09:26 +0800

    asdasdas

--- app/controllers/FPagesController.php
+++ app/controllers/FPagesController.php
@@ -19,7 +19,7 @@ class FPagesController extends \BaseController {
         //dd(\Carbon\Carbon::now()->toDateString());
         //dd(\Carbon\Carbon::now()->tz('Asia/Manila')->toDateString());
         $announcements = Announcement::where('receivers_group', 'LIKE', '{"all":1%')
-                        ->where('updated_at', '>=', \Carbon\Carbon::now()->tz('UTC')->toDateString())
+                        ->where('updated_at', '>=', \Carbon\Carbon::now()->tz('Asia/Manila')->toDateString())
                         ->orderBy('created_at', 'DESC')->get();
         return View::make('front-end.monitor', compact('announcements'));
     }
